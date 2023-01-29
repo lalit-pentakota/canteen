40166e7867cfaa711700ca495ed65c9da631df7a
lalit-pentakota <123749984+lalit-pentakota@users.noreply.github.com>
2023-01-29 16:49:48 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,16 @@
+
+function validate() {
+    var username = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+   {
+        alert("Login successfully");
+        alert ("Please Place your order");
+        window.location="ss.html";
+        return false;
+    }
+}
+function response()
+{
+    alert ("Please login first to Explore our Menu");
+    window.location="login.html";
+}
